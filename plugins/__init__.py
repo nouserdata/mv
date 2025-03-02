f277f425a2aa296813211571261d1b63b8972fab
nouserdata <govindkumary2066@gmail.com>
2025-03-02 22:23:47 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,10 +1,5 @@
 from pyrogram import filters, Client as bot
-from modules import (
-    awadhfree, ifasfree, verbalfree, cdsfree, icsfree, pw, khan, kd, cp, neon,
-    appx_master, testlivefree, utk, kaksha, pwfree, khanfree, iq,
-    vision, nidhi, cpfree, allen, iqfree, ifas, pathfree,
-    allenv2, abhinavfree, vajiram, qualityfree, jrffree, cw
-)
+from modules import 
 import master.key as key, msg
 from config import Config
 from database import db
